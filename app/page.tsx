bcf19c39522d7ce7075b68299a64173ceee76080
vitdev77 <vit.dev.77@gmail.com>
2025-09-24 00:03:17 +0300

    24092025_commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <h1>
-      Home <Button variant={"outline"}>Корзина</Button>
-    </h1>
-  );
+  return <h1>Главная</h1>;
 }

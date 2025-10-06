be6d28bfbea3217982155a3374d883d745efa437
vitdev77 <vitdev77@gmail.com>
2025-10-06 16:51:17 +0300

    06102025_002_commit

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import { Nunito } from "next/font/google";
+
+import "./globals.css";
+
+const nunito = Nunito({
+  subsets: ["cyrillic"],
+  variable: "--font-nunito",
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" data-scroll-behavior="smooth">
+      <head>
+        <link data-rh="true" rel="icon" href="/logo.svg" />
+      </head>
+      <body className={`${nunito.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
